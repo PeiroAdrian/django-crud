8a49ec6bfd97b2e505262572eb917484009f36a9
Adrian Peiro Tapia <AdrohaPeiro@gmail.com>
2023-11-20 22:06:41 -0700

    final

--- django-crud-react/client/src/pages/TaskFormPage.jsx
+++ django-crud-react/client/src/pages/TaskFormPage.jsx
@@ -1,31 +1,84 @@
-import { useForm } from 'react-hook-form'
-import {createTask} from '../api/tasks.api'
-import {useNavigate} from 'react-router-dom'
+import { useEffect } from 'react'
+import { get, useForm } from 'react-hook-form'
+import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.api'
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-hot-toast'
 
 export function TaskFormPage() {
 
-    const { register, handleSubmit, formState: {errors} } = useForm()
-    const navigate = useNavigate()
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm()
+    const navigate = useNavigate();
+    const params = useParams()
 
     const onSubmit = handleSubmit(async data => {
-        await createTask(data)
-        navigate('/tasks')
+        if (params.id) {
+            await updateTask(params.id, data)
+            toast.success('Task updated', {
+                position: 'bottom-right',
+                style: {
+                    background: "#101010",
+                    color: "#fff"
+                }
+            })
+        } else {
+            await createTask(data)
+            toast.success('Task created', {
+                position: 'bottom-right',
+                style: {
+                    background: "#101010",
+                    color: "#fff"
+                }
+            })
+        }
+        navigate("/tasks")
     })
 
+    useEffect(() => {
+        async function loadTask() {
+            if (params.id) {
+                const { data: { title, description } } = await getTask(params.id);
+                setValue("title", title);
+                setValue("description", description);
+            }
+        }
+        loadTask()
+    }, [])
+
     return (
-        <div>
+        <div className='max-w-xl mx-auto'>
             <form onSubmit={onSubmit}>
                 <input type="text" placeholder="title"
                     {...register("title", { required: true })}
+                    className='bg-zinc-700 p-3 rounded-lg block w-full mb-3'
                 />
                 {errors.title && <span>Ttile is required</span>}
 
                 <textarea rows="3" placeholder="Description"
                     {...register("description", { required: true })}
+                    className='bg-zinc-700 p-3 rounded-lg block w-full mb-3'
                 ></textarea>
                 {errors.description && <span>Description is required</span>}
-                <button>Save</button>
+                <button className='bg-indigo-500 p-3 rounded-lg block w-full mt-3'>Save</button>
             </form>
+
+            {params.id && (
+            <div className='flex justify-end'>
+                <button className='bg-red-500 p-3 rounded-lg w-48 mt-3' onClick={async () => {
+                const accepted = window.confirm("Are you sure?")
+                if (accepted) {
+                    await deleteTask(params.id);
+                    toast.success('Task deleted', {
+                        position: 'bottom-right',
+                        style: {
+                            background: "#101010",
+                            color: "#fff",
+                        },
+                    });
+                    navigate('/tasks');
+                }
+            }}>Delete</button>
+            </div>
+            )}
         </div>
-    )
+    );
 }
